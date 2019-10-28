0ce2cadabdbfe4aca1df623ab1eddcb81c4dc5f8
Steven G. Harms <steven.harms@flatironschool.com>
2019-10-28 12:48:28 -0400

    Update specs to use match matcher

--- spec/rack_mechanics_spec.rb
+++ spec/rack_mechanics_spec.rb
@@ -1,20 +1,19 @@
 require_relative './spec_helper'
 
-describe "App" do
-
+describe 'App' do
   it 'wishes user a good morning in the morning' do
-    allow(Time).to receive(:now){
-      Time.new(2015,11,27,9,30)
+    allow(Time).to receive(:now) {
+      Time.new(2015, 11, 27, 9, 30)
     }
     get '/'
-    expect(last_response.body).to include("Morning")
+    expect(last_response.body).to match(/Morning/i)
   end
 
   it 'wishes user a good afternoon in the afternoon' do
-    allow(Time).to receive(:now){
-      Time.new(2015,11,27,14,30)
+    allow(Time).to receive(:now) {
+      Time.new(2015, 11, 27, 14, 30)
     }
     get '/'
-    expect(last_response.body).to include("Afternoon")
+    expect(last_response.body).to match(/Afternoon/i)
   end
 end
